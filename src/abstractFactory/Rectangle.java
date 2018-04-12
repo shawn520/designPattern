b5ad337f7437cfd1d31b6baae38271864b9ea77b
shawn <liushawn@outlook.com>
2018-04-12 13:03:11 +0800

    抽象工厂模式的实现

---
+++ src/abstractFactory/Rectangle.java
@@ -0,0 +1,12 @@
+package abstractFactory;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("画一个三角形");
+
+	}
+
+}
